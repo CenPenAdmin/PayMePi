1209400ca1bd6c0d373333a0e64a87d4fbf0f111
Michael Colledge <CenPenSystem@outlook.com>
2025-08-03 20:55:00 -0400

    Fix ngrok browser warning header issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'ngrok-skip-browser-warning']
 }));
 
 // Handle preflight requests for all routes
