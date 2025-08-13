5d9cd9ffc800fb8c09499a13fa16ff2f4a6c2843
Michael Colledge <CenPenSystem@outlook.com>
2025-08-13 08:38:01 -0400

    update ngrok tunnel

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
         const allowedOrigins = [
             'http://localhost:3000',
             'https://CenPenAdmin.github.io',
-            'https://37bf96dd43a1.ngrok-free.app'
+            'https://ad7b482e9013.ngrok-free.app'
         ];
         
         const allowedPatterns = [
@@ -1751,7 +1751,7 @@ app.get('/health', (req, res) => {
             allowedOrigins: [
                 'http://localhost:3000',
                 'https://CenPenAdmin.github.io',
-                'https://37bf96dd43a1.ngrok-free.app'
+                'https://ad7b482e9013.ngrok-free.app'
             ]
         }
     });
